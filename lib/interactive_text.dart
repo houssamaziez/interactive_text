58ab1d4410de449b615776a992789038aa5390c1
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2024-12-18 14:31:11 +0100

    Initial commit

---
+++ lib/interactive_text.dart
@@ -0,0 +1,9 @@
+library interactive_text;
+library interactive_text;
+
+export 'src/interactive_text_screen.dart';
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
